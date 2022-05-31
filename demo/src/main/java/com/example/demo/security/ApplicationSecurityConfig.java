a8f98c03a2c54a943e4232aa6dc951606cd6d092
Sbaah <sbaah12@gmail.com>
2022-05-31 11:15:23 -0400

    Csrf was re-activated due to using postman for testing. It will be activated when browser process application is implemented.

--- demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 import static com.example.demo.security.ApplicationUserRole.*;
@@ -32,14 +33,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http
-               .csrf().disable() // TODO: Will lean this in detail in the next section
+               // Header title: XSRF-TOKEN
+               // .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+               // .and()
+               .csrf().disable()
                .authorizeRequests()
                .antMatchers("/","index","/css*","js/*").permitAll()
                .antMatchers("/api/**").hasRole(STUDENT.name())
-//               .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                .anyRequest()
                .authenticated().and()
                .httpBasic();
